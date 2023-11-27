3f08d276183ee13d0576deb81cdaa5a268f8d570
Richard Choque <richard.choque@globallogic.com>
2023-11-27 18:58:44 -0300

    added files in project

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+
+//Components
+import { AppComponent } from './app.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListarProductosComponent,
+    CrearProductoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ProductoService } from '../../services/producto.service';
+import { Producto } from '../../models/producto';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-listar-productos',
+  templateUrl: './listar-productos.component.html',
+  styleUrl: './listar-productos.component.css'
+})
+export class ListarProductosComponent {
+  listProductos: Producto[] = [];
+
+  constructor(private _productoService: ProductoService,
+              private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+    this.obtenerProductos();
+  }
+
+  obtenerProductos() {
+    this._productoService.getProductos().subscribe(data => {
+      console.log(data);
+      this.listProductos = data;
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  eliminarProducto(id: any) {
+    this._productoService.eliminarProducto(id).subscribe(data => {
+      this.toastr.error('El producto fue eliminado con exito', 'Producto Eliminado');
+      this.obtenerProductos();
+    }, error => {
+      console.log(error);
+    })
+  }
+
+}
